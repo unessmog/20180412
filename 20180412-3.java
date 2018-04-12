602bd9cbe88377dd0af110a212ab3ed3c6fefbf1
brian <nticishental@pokemail.net>
2018-04-12 09:29:06 -0400

    initializing repo

---
+++ 20180412-3.java
@@ -0,0 +1,19 @@
+import javax.swing.*
+
+public class PromptThree {
+    public void method inputArea {
+        for (i = 1; i <= 100; i++) {
+            set variable to true;
+            if i is divisible by 3 {
+                print "Fizz";
+                set variable to false;
+            }
+            if i is divisible by 5 {
+                print "Buzz";
+            set variable to false;
+            }
+            if variable, print i;
+            print a newline;
+        }
+    }
+}
---
+++ 20180412-4.java
@@ -0,0 +1,22 @@
+import javax.io.*
+
+public class PromptFour {
+    public void method joinData(String username, String password) {
+    }
+    
+    public void method write(File filename, String data) {
+        for (i = 1; i <= 100; i++) {
+            set variable to true;
+            if i is divisible by 3 {
+                print "Fizz";
+                set variable to false;
+            }
+            if i is divisible by 5 {
+                print "Buzz";
+            set variable to false;
+            }
+            if variable, print i;
+            print a newline;
+        }
+    }
+}
